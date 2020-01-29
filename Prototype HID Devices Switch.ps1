4e04879fed65f9ee8821b6ac69fd193861e819dc
Michał B <MadTiger2409@users.noreply.github.com>
2020-01-29 12:10:13 +0100

    Add files via upload

---
+++ Prototype HID Devices Switch.ps1
@@ -0,0 +1,51 @@
+# Hide any errors
+$ErrorActionPreference = 'silentlycontinue'
+
+# Stage up to Administrator privileges
+If (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+	$Arguments = "& '" + $MyInvocation.mycommand.definition + "'"
+	Start-Process powershell -Verb runAs -ArgumentList $Arguments
+	Break
+}
+
+# Get HID Devices that are needed to be disabled/enableed
+$VendorDevices = (Get-PnpDevice -FriendlyName "*HID-compliant vendor*" -Class "HIDClass")
+$ConsumerDevices = (Get-PnpDevice -FriendlyName "*HID-compliant consumer*" -Class "HIDClass")
+
+
+# Disable/enable first part of the devices
+foreach ($item in $VendorDevices) {
+
+	if ($item.Status -eq "OK") {
+		$id = $item.InstanceId
+		$name = $item.FriendlyName
+		Disable-PnpDevice "$id" -Confirm:$false
+		Write-Output "$name disabled"
+	}
+	elseif ($item.Status -eq "Error") {
+		$id = $item.InstanceId
+		$name = $item.FriendlyName
+		Enable-PnpDevice "$id" -Confirm:$false
+		Write-Output "$name enabled"
+	}
+}
+
+# Disable/enable second part of the devices
+foreach ($item in $ConsumerDevices) {
+
+	if ($item.Status -eq "OK") {
+		$id = $item.InstanceId
+		$name = $item.FriendlyName
+		Disable-PnpDevice "$id" -Confirm:$false
+		Write-Output "$name disabled"
+	}
+	elseif ($item.Status -eq "Error") {
+		$id = $item.InstanceId
+		$name = $item.FriendlyName
+		Enable-PnpDevice "$id" -Confirm:$false
+		Write-Output "$name enabled"
+	}
+}
+
+# Keep window open after operation
+$host.enternestedprompt()
